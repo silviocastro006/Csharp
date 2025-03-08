d2efa4cea3facf100a62de73121d92ceca5ecd97
Sílvio Ricardo <silviocastro006@gmail.com>
2025-03-07 21:37:18 -0300

    atualização

--- Serenatto/obj/Debug/net8.0/Serenatto.AssemblyInfo.cs
+++ Serenatto/obj/Debug/net8.0/Serenatto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Serenatto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e340392b107eb4e1ce8d9d177403475d3ff004")]
 [assembly: System.Reflection.AssemblyProductAttribute("Serenatto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Serenatto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
