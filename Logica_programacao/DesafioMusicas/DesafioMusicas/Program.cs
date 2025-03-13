e5949026ec4ff1e2eac2523775b2f73ce8201aa7
Sílvio Ricardo <silviocastro006@gmail.com>
2025-03-12 23:43:51 -0300

    atualização

--- Logica_programacao/DesafioMusicas/DesafioMusicas/Program.cs
+++ Logica_programacao/DesafioMusicas/DesafioMusicas/Program.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using NPOI;
 using NPOI.SS.UserModel;
 using DesafioMusica.Models.Musica;
+using NPOI.SS.Formula.Functions;
 
 namespace DesafioMusica.Principal;
 
@@ -17,7 +18,12 @@ class Program
     static void Main(string[] args)
     {
         ImportarDados();
-        Exercicio01();
+        //Exercicio01();
+        //Exercicio02();
+        //Exercicio03();
+        //Exercicio04();
+        //Exercicio05();
+        Exercicio06();
     }
 
 
@@ -64,5 +70,151 @@ class Program
         Console.WriteLine($"A maior duração é a {maiorDuracao.Nome} de {maiorDuracao.Duracao:F2} minutos");
     }
 
+    public static void Exercicio02()
+    {
+        var artistasUnicos = musicas.DistinctBy(x => x.Artista).Count();
+        Console.WriteLine($"No total temos {artistasUnicos} artistas únicos");
+    }
 
+    public static void Exercicio03()
+    {
+        var lancamentoAlbuns = musicas
+                               .GroupBy(x => new{
+                                    mes = x.DataLancamento.Date.Month,
+                                    ano = x.DataLancamento.Date.Year
+                               })
+                               .Select(x => new{
+                                    data = new DateTime(x.Key.ano,x.Key.mes,1),
+                                    quantidade = x.Select(y => y.DataLancamento).Count()
+                               })
+                               .OrderBy(x => x.data.Year)
+                               .ThenBy(x => x.data.Month);
+
+
+        foreach (var lancamento in lancamentoAlbuns)
+        {
+            Console.WriteLine($"{lancamento.data.ToString("MMMM").ToUpper(),-10}{"/",-5}{lancamento.data.ToString("yyyy"),-10}: {lancamento.quantidade, 5}");
+        }
+
+        
+    }
+
+    public static void Exercicio04()
+    {
+        
+        Console.WriteLine("=============================     A     ================================\n");
+
+        var generoComMaisMusicas = musicas
+                                   .GroupBy(x => x.Genero)
+                                   .Select(x => new{
+                                        nome = x.Key,
+                                        quantidade = x.Count()
+                                   })
+                                   .OrderByDescending(x => x.quantidade)
+                                   .Take(5);
+                                   
+
+        int posicao = 1;
+
+        foreach (var coiso in generoComMaisMusicas)
+        {
+            Console.WriteLine($"{posicao, -4}º Lugar: {coiso.nome, -25} - {coiso.quantidade, -5} musicas");
+            posicao++;
+        }
+
+        Console.WriteLine("=============================     B     ================================\n");
+
+
+        var albunsComMaisMusicas = musicas
+                                   .GroupBy(x => x.Album)
+                                   .Select(x => new{
+                                        nome = x.Key,
+                                        quantidade_Musicas = x.Select(y => y.Nome).Count()
+                                   })
+                                   .OrderByDescending(x => x.quantidade_Musicas)
+                                   .Take(3);
+                                   
+
+        int top = 1;
+        foreach (var coiso in albunsComMaisMusicas)
+        {
+            Console.WriteLine($"{top, -4}º Lugar: {coiso.nome, -25} - {coiso.quantidade_Musicas, -5} musicas");
+            top++;
+        }
+
+            Console.WriteLine("=============================     C     ================================\n");
+
+
+        var paisesComMaisMusicas =  musicas
+                                    .GroupBy(x => x.Pais)
+                                    .Select(x => new{
+                                            nome_pais = x.Key,
+                                            quantidade_Musicas = x.Select(y => y.Nome).Count()
+                                    })
+                                    .OrderByDescending(x => x.quantidade_Musicas)
+                                    .Take(5);
+                                   
+
+        int topy = 1;
+        foreach (var coiso in paisesComMaisMusicas)
+        {
+            Console.WriteLine($"{topy, -4}º Lugar: {coiso.nome_pais, -25} - {coiso.quantidade_Musicas, -5} musicas");
+            topy++;
+        }               
+
+        
+    }
+
+    public static void Exercicio05()
+    {   
+        var GravadorasComMaisMusicas = musicas
+                                       .GroupBy(x => x.Gravadora)
+                                       .Select(x => new{
+                                            nome_gravadora = x.Key,
+                                            quantidade_Musicas = x.Select(y => y.Gravadora).Count()
+                                       })
+                                       .OrderByDescending(x => x.quantidade_Musicas);
+                                   
+
+        int top = 1;
+
+        Console.WriteLine($"No total temos {GravadorasComMaisMusicas.CountBy(x => x.nome_gravadora).Count()} gravadoras");
+
+        foreach (var coiso in GravadorasComMaisMusicas)
+        {
+            Console.WriteLine($"{top, -4}º Lugar: {coiso.nome_gravadora, -25} - {coiso.quantidade_Musicas, -5} musicas");
+            top++;
+        }
+    }
+
+     public static void Exercicio06()
+    {   
+        // var IdiomaComMaisMusicas = musicas
+        //                           .GroupBy(x => new{
+        //                                 mes = x.DataLancamento.Date.Month,
+        //                                 ano = x.DataLancamento.Date.Year,
+        //                                 idioma = x.Idioma
+        //                           })
+        //                           .Select(x => new{
+        //                                 data = new DateTime(x.Key.ano,x.Key.mes,1),
+        //                                 quantidade = x.Select(y => y.Idioma).Count()
+        //                            })
+        //                           .OrderByDescending(x => x.data.Year)
+        //                           .ThenByDescending(x => x.data.Month)
+        //                           .ThenByDescending(x => x.quantidade)
+        //                           .Select(x => new{
+                                        
+        //                            })
+                                  
+        
+        // foreach (var lancamento in IdiomaComMaisMusicas)
+        // {
+        //     Console.WriteLine($"{lancamento.data.ToString("MMMM").ToUpper(),-10}{"/",-5}{lancamento.data.ToString("yyyy"),-10}: {lancamento.quantidade, 5}");
+        // }
+                                   
+
+
+        
+        
+    }
 }
